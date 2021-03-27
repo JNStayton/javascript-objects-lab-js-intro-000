a6d49123f58f74526531a8b9c6c07e30acde2d31
Jessica Stayton <jessica.stayton@gmail.com>
2021-03-27 19:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {food: "amount"};
 
 function updateObjectWithKeyAndValue(recipes, food, amount) {
-  recipes[food: amount];
+  Object.assign(recipes, [food: amount]);
   return recipes;
 }
