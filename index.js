d5ced8416451b179bda7335550c936b15ea7872b
Jessica Stayton <jessica.stayton@gmail.com>
2021-03-27 19:34:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   Object.assign(object, {[key]: value});
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj  = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
